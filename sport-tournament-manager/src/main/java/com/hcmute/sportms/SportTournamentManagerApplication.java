e2982a56e75024b6a5ffafc50ee3a45fd45bc528
Nguyen Tran Quoc Thi <thi272005@gmail.com>
2026-06-23 01:32:19 +0700

    tạo project web spring boot + fix file sql 03

---
+++ sport-tournament-manager/src/main/java/com/hcmute/sportms/SportTournamentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.sportms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTournamentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportTournamentManagerApplication.class, args);
+	}
+
+}
---
+++ sport-tournament-manager/src/test/java/com/hcmute/sportms/SportTournamentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmute.sportms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportTournamentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
